4cdbc427c7907961574edf350be52c3ed3612877
赵之骏 <13405870807@163.Com>
2025-08-03 20:33:51 +0800

    refactor(graphics): 调整多个预制件的排序层并删除冗余代码
    -调整了多个预制件的 SortingLayerID 和 SortingLayer 值
    - 删除了未使用的 BulletCollide 和 BulletCollode 类
    - 移除了相关的元数据文件

---
+++ Assets/Scripts/TipSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TipSystem : MonoBehaviour
+{
+    public GameObject Tip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
